902d454ec411bb86599193fff9edd6e0fb49b317
mfido <mikolaj.fido@gmail.com>
2020-08-15 13:27:35 +0100

    change course

--- sailboat.py
+++ sailboat.py
@@ -5,4 +5,6 @@ class Sailboat(object):
         
     def get_heading(self):
         return self._heading  
-      
+    
+    def change_course(self, course_diff):
+        self._heading = (self._heading + course_diff) % 360.0
--- test/sailboat_test.py
+++ test/sailboat_test.py
@@ -1,4 +1,5 @@
 import unittest
+from parameterized import parameterized
 from sailboat import Sailboat
 
 class SailboatTest(unittest.TestCase):
@@ -7,6 +8,25 @@ class SailboatTest(unittest.TestCase):
         sailboat = Sailboat(0.0)
         self.assertEqual(sailboat.get_heading(), 0.0)
 
+    @parameterized.expand([
+        (0.0, 10.0, 10.0),
+        (0.0, 90.0, 90.0),
+        (270.0, 100.0, 10.0)
+    ])        
+    def test_change_course_to_starboard(self, initial_heading, course_diff, end_heading):
+        sailboat = Sailboat(initial_heading)
+        sailboat.change_course(course_diff)
+        self.assertEqual(sailboat.get_heading(), end_heading)
+        
+    @parameterized.expand([
+        (0.0, -10.0, 350.0),
+        (0.0, -90.0, 270.0),
+        (270.0, -100.0, 170.0)
+    ])        
+    def test_change_course_to_portside(self, initial_heading, course_diff, end_heading):
+        sailboat = Sailboat(initial_heading)
+        sailboat.change_course(course_diff)
+        self.assertEqual(sailboat.get_heading(), end_heading)
 
 if __name__ == "__main__":
     unittest.main()
