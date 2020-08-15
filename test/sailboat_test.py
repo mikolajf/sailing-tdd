a641b4545e60f35785af5dd19db4dbffc6b15521
mfido <mikolaj.fido@gmail.com>
2020-08-15 17:34:27 +0100

    rhumbs

--- test/sailboat_test.py
+++ test/sailboat_test.py
@@ -7,6 +7,15 @@ class SailboatTest(unittest.TestCase):
     def test_initial_heading(self):
         sailboat = Sailboat(0.0)
         self.assertEqual(sailboat.get_heading(), 0.0)
+    
+    @parameterized.expand([
+        (0.0, 'N'),
+        (350.0, 'N'),
+        (10.0, 'N')
+    ])      
+    def test_initial_rhumb(self, initial_heading, expected_rhumb):
+        sailboat = Sailboat(initial_heading)
+        self.assertEqual(sailboat.get_rhumb(), expected_rhumb)
 
     @parameterized.expand([
         (0.0, 10.0, 10.0),
